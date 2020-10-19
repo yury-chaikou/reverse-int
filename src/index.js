636fb64a52711f27b707fe0dc4c3ba1602e3f40f
yury-chaikou <68887857+yury-chaikou@users.noreply.github.com>
2020-10-19 22:37:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-module.exports = function reverse (n) {
-    module.exports = function reverse(n) {
-        let num = Math.abs(n)
-        let str = String(num);
-        let revers = '';
-        for (let i = 0; i < str.length; i++) {
-            revers = (str[i]) + revers;
-        }
-        return Number(revers);
+module.exports = function reverse(n) {
+    let num = Math.abs(n)
+    let str = String(num);
+    let revers = '';
+    for (let i = 0; i < str.length; i++) {
+        revers = (str[i]) + revers;
     }
+    return Number(revers);
 }
